c43a4dc9e3b1a26585ac6eba7e95369fbf056e98
saul6000 <61443057+saul6000@users.noreply.github.com>
2021-08-22 22:15:36 -0500

    Add files via upload

---
+++ ns3_Dependencias.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install git mercurial
+sudo apt-get install g++ 
+sudo apt-get install make cmake
+sudo apt-get install python3 python3-dev pkg-config sqlite3
+sudo apt-get install python3-setuptools
+sudo apt-get install gir1.2-goocanvas-2.0 python-gi python-gi-cairo python3-gi python3-gi-cairo python3-pygraphviz gir1.2-gtk-3.0 ipython3  
+sudo apt-get install qt5-default
+sudo apt-get install gdb
+sudo apt-get install python-dev
+sudo apt-get install python3-dev
